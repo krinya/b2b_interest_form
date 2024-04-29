6d2252214c75924b728eb19a6a41cdafb1cece9d
Kristof Menyhert <menyhert.kristof@gmail.com>
2024-04-29 19:08:13 +0200

    Update B2B interest form validation error messages

--- app.py
+++ app.py
@@ -29,32 +29,32 @@ def form_first_step_button_press():
     st.session_state['first_step_button_pressed'] = 1
 
 st.markdown(f'![CleanGo Logo](https://cleango.hu/sitebuild/img/logo-text.svg)')
-st.markdown("# B2B Erdeklodesi Felulet")
-st.markdown(":car: Ha szeretne ajanlatot kapni a CleanGo-tol, kerjuk toltse ki az alabbi formot.")
+st.markdown("# B2B Árajánlat kérő felület")
+st.markdown(":car: Ha szeretnél B2B árajánlatot kapni a CleanGo-tol, akkor töltsd ki az alabbi ürlapot, és mi pár napon belül felvesszuk veled a kapcsolatot. :car:")
 
 main_cointainer = st.container(border=True)
 
 # Create a form container
 with main_cointainer:
     st.markdown("#### Kapcsolati adatok")
-    st.text_input("**Cegnev**", key="company_name")
-    st.text_input("Kapcsolattarto neve", key="contact_name", placeholder="Vezeteknev Keresztnev")
-    st.text_input("Telefonszam", key="phone_number", placeholder="+36")
-    st.text_input("Email cim", key="email")
-    st.markdown("#### Mosasra vonatkozo adatok")
-    st.number_input("Osszesen mennyi autoval rendelkezel?", key="total_cars", min_value=0, value=None)
-    st.number_input("Hany autot tervezel takarittatni havonta?", key="monthly_cleaning", min_value=0, value=None)
-    st.number_input("Mosas helyszinenek iranyitoszama?", key="location_zip", min_value=1000, max_value = 10000, value=None)
-    list_of_parking_options = ["Fedett", "Szabadteri", "Mindketto"]
-    st.radio("Milyen parkolasi lehetosegekkel rendelkezel?", list_of_parking_options, key="parking_options", index=None)
-    list_of_car_types_options = ["Szemelyauto", "Nem szemelyauto", "Mindketto"]
-    st.radio("Milyen autokat szeretnel takarittatni?", list_of_car_types_options, key="car_types", index=None)
-    st.markdown("#### Egyeb informaciok")
+    st.text_input("Cégnev", key="company_name")
+    st.text_input("Kapcsolattartó neve", key="contact_name", placeholder="Vezetéknév Keresztnév")
+    st.text_input("Telefonszám", key="phone_number", placeholder="+36")
+    st.text_input("Email cím", key="email")
+    st.markdown("#### Mosásra vonatkozó adatok")
+    st.number_input("Összesen mennyi autóval rendelkezel?", key="total_cars", min_value=0, value=None)
+    st.number_input("Hány autót tervezel takaríttatni havonta?", key="monthly_cleaning", min_value=0, value=None)
+    st.number_input("Mosás helyszinének iranyítószáma?", key="location_zip", min_value=1000, max_value = 10000, value=None, placeholder=1234)
+    list_of_parking_options = ["Fedett", "Szabadtéri", "Mindkettő"]
+    st.radio("Milyen parkolási lehetőségekkel rendelkezel?", list_of_parking_options, key="parking_options", index=None)
+    list_of_car_types_options = ["Személyautó", "Nem Személyautó", "Mindkettő"]
+    st.radio("Milyen autókat szeretnél takaríttatni?", list_of_car_types_options, key="car_types", index=None)
+    st.markdown("#### Egyéb informaciok")
     # dropdown
-    list_of_heared_from_us_options = ["Google", "Facebook", "Ismeros", "Egyeb"]
-    st.selectbox("Honnan hallottal rolunk?", list_of_heared_from_us_options, key="heard_from", index=None, placeholder="Kerjuk valassz")
-    st.text_area("Egyeb barmi mas megjegyzes", key="comments")
-    st.button("Arajanlat kerese", on_click=form_send_in_button_press)
+    list_of_heared_from_us_options = ["Google", "Facebook", "Ismerős ajánlotta", "Egyéb"]
+    st.selectbox("Honnan hallottál rólunk?", list_of_heared_from_us_options, key="heard_from", index=None, placeholder="Kerjuk valassz")
+    st.text_area("Egyéb bármi más megjegyzés, speciális kérdés", key="comments", placeholder="Ide írhatod a megjegyzéseidet")
+    st.button("Árajánlat beküldése", on_click=form_send_in_button_press)
 
 if st.session_state['send_in_button_pressed'] == 1:
 
@@ -62,26 +62,26 @@ if st.session_state['send_in_button_pressed'] == 1:
 
     if validate_inputs_true_false:
 
-        st.success("Az ajanlatkeres sikeresen elkuldve. Hamarosan felvesszuk veled a kapcsolatot. Koszonjuk!")
-        st.markdown("Addig tekintse meg a kovetkezo dolgokat: Es ide irhatunk barmit, amit szeretnenk.")
+        st.success("Koszonjuk! Az árajánlat kérést sikeresen megkaptuk. Hamarosan felvesszük veled a kapcsolatot.")
+        st.markdown("Addig nézd meg a kovetkező dolgokat: és ide irhatunk bármit, amit szeretnénk.")
         
         filled_in_data_container = st.container(border=True)
 
         with filled_in_data_container:
-            st.markdown("#### Az elkuldott arajanlatkerese tartalma")
-            st.markdown(f"Cegnev: {st.session_state['company_name']}")
-            st.markdown(f"Kapcsolattarto neve: {st.session_state['contact_name']}")
-            st.markdown(f"Telefonszam: {st.session_state['phone_number']}")
-            st.markdown(f"Email cim: {st.session_state['email']}")
-            st.markdown(f"Osszesen mennyi autoval rendelkezel?: {st.session_state['total_cars']}")
-            st.markdown(f"Hany autot tervezel takarittatni havonta?: {st.session_state['monthly_cleaning']}")
-            st.markdown(f"Mosas helyszinenek iranyitoszama?: {st.session_state['location_zip']}")
-            st.markdown(f"Milyen parkolasi lehetosegekkel rendelkezel?: {st.session_state['parking_options']}")
-            st.markdown(f"Milyen autokat szeretnel takarittatni?: {st.session_state['car_types']}")
-            st.markdown(f"Honnan hallottal rolunk?: {st.session_state['heard_from']}")
-            st.markdown(f"Egyeb barmi mas megjegyzes: {st.session_state['comments']}")
+            st.markdown("#### Az elküldott árajánlat kérés tartalma")
+            st.markdown(f"Cégnev: {st.session_state['company_name']}")
+            st.markdown(f"Kapcsolattartó neve: {st.session_state['contact_name']}")
+            st.markdown(f"Telefonszám: {st.session_state['phone_number']}")
+            st.markdown(f"Email cím: {st.session_state['email']}")
+            st.markdown(f"Összesen mennyi autóval rendelkezel?: {st.session_state['total_cars']}")
+            st.markdown(f"Hány autot tervezel takaríttatni havonta?: {st.session_state['monthly_cleaning']}")
+            st.markdown(f"Mosás helyszinének iranyítószáma?: {st.session_state['location_zip']}")
+            st.markdown(f"Milyen parkolási lehetőségekkel rendelkezel?: {st.session_state['parking_options']}")
+            st.markdown(f"Milyen autókat szeretnél takaríttatni?: {st.session_state['car_types']}")
+            st.markdown(f"Honnan hallottál rólunk?: {st.session_state['heard_from']}")
+            st.markdown(f"Egyéb bármi más megjegyzés, speciális kérdés: {st.session_state['comments']}")
     else:
-        st.error("Az ajanlatkeres nem sikerult, mert nem toltotte ki helyesen a mezoket.")
+        st.error("Az árajánlat kérés nem sikerült, kérjük ellenőrizd az adatokat és próbáld újra. Köszönjük!")
 
     st.session_state['send_in_button_pressed'] = 0
 
--- utils/app_functions.py
+++ utils/app_functions.py
@@ -6,46 +6,46 @@ import base64
 
 def validate_inputs():
     if st.session_state['company_name'] == "":
-        st.error("Cegnev nem lehet ures")
+        st.error("Cégnev nem lehet üres. Kérlek add meg a céged nevét.")
         return False
     if st.session_state['contact_name'] == "":
-        st.error("Kapcsolattarto neve nem lehet ures")
+        st.error("Kapcsolattartó neve nem lehet üres. Kérlek add meg a neved.")
         return False
     if st.session_state['phone_number'] == "":
-        st.error("Telefonszam nem lehet ures")
+        st.error("Telefonszám nem lehet üres. Kérlek add meg a telefonszámod.")
         return False
     # telefonszam needs to contains only numbers or "+" sign
     if not st.session_state['phone_number'].replace("+", "").isdigit():
-        st.error("Telefonszam nem megfelelo, csak szamokat es '+'-t tartalmazhat")
+        st.error("Telefonszám nem megfelelő, csak számokat és '+'-t tartalmazhat.")
         return False
     if st.session_state['email'] == "":
-        st.error("Email cim nem lehet ures")
+        st.error("Email cim nem lehet üres. Kérlek add meg az email cimedet.")
         return False
     if st.session_state['email'].find("@") == -1:
-        st.error("Email cim nem megfelelo, nem tartalmaz @ karaktert")
+        st.error("Email cím nem megfelelő, nem tartalmaz @ (kukac) karaktert.")
         return False
     if st.session_state['email'].find(".") == -1:
-        st.error("Email cim nem megfelelo, nem tartalmaz . karaktert")
+        st.error("Email cím nem megfelelő, nem tartalmaz . (pont) karaktert.")
         return False
     if st.session_state['email'].find(" ") != -1:
-        st.error("Email cim nem megfelelo, nem tartalmazhat szokozt")
+        st.error("Az email cím nem megfelelő, nem tartalmazhat szóközt.")
         return False
     if st.session_state['total_cars'] == "" or st.session_state['total_cars'] is None:
-        st.error("Osszesen mennyi autoval rendelkezel? nem lehet ures")
+        st.error("Az 'Összesen mennyi autóval rendelkezel?' mező nem lehet üres")
         return False
     if st.session_state['monthly_cleaning'] == "" or st.session_state['monthly_cleaning'] is None:
-        st.error("Hany autot tervezel takarittatni havonta? nem lehet ures")
+        st.error("A 'Hány autót tervezel takaríttatni havonta?' mező nem lehet ures")
         return False
     if st.session_state['location_zip'] == "" or st.session_state['location_zip'] is None:
-        st.error("Mosas helyszinenek iranyitoszama? nem lehet ures")
+        st.error("A Mosás helyszínenek iranyítoszama nem lehet üres mező.")
         return False
     if st.session_state['parking_options'] == "" or st.session_state['parking_options'] is None:
-        st.error("Milyen parkolasi lehetosegekkel rendelkezel? nem lehet ures")
+        st.error("A 'Milyen parkolási lehetősegekkel rendelkezel?' mező nem lehet üres.")
         return False
     if st.session_state['car_types'] == "" or st.session_state['car_types'] is None:
-        st.error("Milyen autokat szeretnel takarittatni? nem lehet ures")
+        st.error("A 'Milyen autokat szeretnel takarittatni?' mező nem lehet üres.")
         return False
     if st.session_state['heard_from'] == "" or st.session_state['heard_from'] is None:
-        st.error("Honnan hallottal rolunk? nem lehet ures")
+        st.error("A 'Honnan hallottál rolunk?' mező nem lehet üres.")
         return False
     return True
