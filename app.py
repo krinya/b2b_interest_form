9e11c176714c67836cb0cc451c5801514eec7bf2
Kristof Menyhert <menyhert.kristof@gmail.com>
2024-04-29 06:39:21 +0200

    Refactor form validation logic and update success message in app.py

--- app.py
+++ app.py
@@ -20,15 +20,6 @@ def form_first_step_button_press():
     st.session_state['first_step_button_pressed'] = 1
 
 def validate_inputs():
-    if st.session_state['email'] == "":
-        st.error("Email cim nem lehet ures")
-        return False
-    if st.session_state['email'].find("@") == -1:
-        st.error("Email cim nem megfelelo, nem tartalmaz @ karaktert")
-        return False
-    if st.session_state['email'].find(".") == -1:
-        st.error("Email cim nem megfelelo, nem tartalmaz . karaktert")
-        return False
     if st.session_state['company_name'] == "":
         st.error("Cegnev nem lehet ures")
         return False
@@ -37,6 +28,19 @@ def validate_inputs():
         return False
     if st.session_state['phone_number'] == "":
         st.error("Telefonszam nem lehet ures")
+        return False
+    # telefonszam needs to contains only numbers or "+" sign
+    if not st.session_state['phone_number'].replace("+", "").isdigit():
+        st.error("Telefonszam nem megfelelo, csak szamokat es '+'-t tartalmazhat")
+        return False
+    if st.session_state['email'] == "":
+        st.error("Email cim nem lehet ures")
+        return False
+    if st.session_state['email'].find("@") == -1:
+        st.error("Email cim nem megfelelo, nem tartalmaz @ karaktert")
+        return False
+    if st.session_state['email'].find(".") == -1:
+        st.error("Email cim nem megfelelo, nem tartalmaz . karaktert")
         return False
     if st.session_state['total_cars'] == "" or st.session_state['total_cars'] is None:
         st.error("Osszesen mennyi autoval rendelkezel? nem lehet ures")
@@ -83,56 +87,56 @@ with main_cointainer:
     list_of_heared_from_us_options = ["Google", "Facebook", "Ismeros", "Egyeb"]
     st.selectbox("Honnan hallottal rolunk?", list_of_heared_from_us_options, key="heard_from", index=None, placeholder="Kerjuk valassz")
     st.text_area("Egyeb barmi mas megjegyzes", key="comments")
-    st.button("Kovetkezo lepes", on_click=form_first_step_button_press)
+    st.button("Arajanlat kerese", on_click=form_first_step_button_press)
 
 if st.session_state['first_step_button_pressed'] == 1:
 
     validate_inputs_true_false = validate_inputs()
 
-    filled_in_data_container = st.container(border=True)
-    with filled_in_data_container:
-        st.write("Ajanlatkeres tartalma:")
-        st.write("Cegnev: ", st.session_state['company_name'])
-        st.write("Kapcsolattarto neve: ", st.session_state['contact_name'])
-        st.write("Telefonszam: ", st.session_state['phone_number'])
-        st.write("Email cim: ", st.session_state['email'])
-        st.write("Osszesen mennyi autoval rendelkezel?: ", st.session_state['total_cars'])
-        st.write("Hany autot tervezel takarittatni havonta?: ", st.session_state['monthly_cleaning'])
-        st.write("Mosas helyszinenek iranyitoszama?: ", st.session_state['location_zip'])
-        st.write("Milyen parkolasi lehetosegekkel rendelkezel?: ", st.session_state['parking_options'])
-        st.write("Milyen autokat szeretnel takarittatni?: ", st.session_state['car_types'])
-        st.write("Honnan hallottal rolunk?: ", st.session_state['heard_from'])
-        st.write("Egyeb barmi mas megjegyzes: ", st.session_state['comments'])
-        st.button("Kuld", on_click=form_send_in_button_press)
+    if validate_inputs_true_false:
+        st.success("Az ajanlatkeres sikeresen elkuldve. Hamarosan felvesszuk onnel a kapcsolatot. Koszonjuk!")
+        st.markdown("Addig tekintse meg a kovetkezo dolgokat: Es ide irhatunk barmit, amit szeretnenk.")
+        st.markdown("***")
+        filled_in_data_container = st.container(border=True)
 
-    if st.session_state['send_in_button_pressed'] == 1:
-        if validate_inputs_true_false:
-            st.success("Az ajanlatkeres sikeresen elkuldve.")
-        else:
-            st.error("Az ajanlatkeres nem sikerult, mert nem toltotte ki helyesen a mezoket.")
-        st.session_state['send_in_button_pressed'] = 0
+        with filled_in_data_container:
+            st.markdown("#### Az elkuldott arajanlatkerese tartalma")
+            st.markdown(f"Cegnev: {st.session_state['company_name']}")
+            st.markdown(f"Kapcsolattarto neve: {st.session_state['contact_name']}")
+            st.markdown(f"Telefonszam: {st.session_state['phone_number']}")
+            st.markdown(f"Email cim: {st.session_state['email']}")
+            st.markdown(f"Osszesen mennyi autoval rendelkezel?: {st.session_state['total_cars']}")
+            st.markdown(f"Hany autot tervezel takarittatni havonta?: {st.session_state['monthly_cleaning']}")
+            st.markdown(f"Mosas helyszinenek iranyitoszama?: {st.session_state['location_zip']}")
+            st.markdown(f"Milyen parkolasi lehetosegekkel rendelkezel?: {st.session_state['parking_options']}")
+            st.markdown(f"Milyen autokat szeretnel takarittatni?: {st.session_state['car_types']}")
+            st.markdown(f"Honnan hallottal rolunk?: {st.session_state['heard_from']}")
+            st.markdown(f"Egyeb barmi mas megjegyzes: {st.session_state['comments']}")
+    else:
+        st.error("Az ajanlatkeres nem sikerult, mert nem toltotte ki helyesen a mezoket.")
+    st.session_state['send_in_button_pressed'] = 0
 
-st.markdown("---")
+# st.markdown("---")
 
-st.markdown("The form structure was last updated on:")
-# get the latest date of the files within the pages folder
-pages_folder = os.path.join(os.getcwd())
-files = os.listdir(pages_folder)
-files.sort(reverse=True)
+# st.markdown("The form structure was last updated on:")
+# # get the latest date of the files within the pages folder
+# pages_folder = os.path.join(os.getcwd())
+# files = os.listdir(pages_folder)
+# files.sort(reverse=True)
 
-#create a max_date variable with a default value
-max_date = datetime.datetime(2021, 1, 1, 0, 0, 0, 0)
-# get the modification time of a file
-for file in files:
-    modification_time = os.path.getmtime(os.path.join(pages_folder, file))
-    # convert timestamp to datetime and local is set to Europe/Amsterdam
-    last_update = datetime.datetime.fromtimestamp(modification_time)
-    # modifiy the max_date if the last_update is greater than the max_date
-    if last_update > max_date:
-        max_date = last_update
+# #create a max_date variable with a default value
+# max_date = datetime.datetime(2021, 1, 1, 0, 0, 0, 0)
+# # get the modification time of a file
+# for file in files:
+#     modification_time = os.path.getmtime(os.path.join(pages_folder, file))
+#     # convert timestamp to datetime and local is set to Europe/Amsterdam
+#     last_update = datetime.datetime.fromtimestamp(modification_time)
+#     # modifiy the max_date if the last_update is greater than the max_date
+#     if last_update > max_date:
+#         max_date = last_update
 
-# foromat max_date to show only yyyy-mm-dd
-max_date_yyyy_mm_dd = max_date.strftime("%Y-%m-%d")
-st.markdown(max_date_yyyy_mm_dd)
+# # foromat max_date to show only yyyy-mm-dd
+# max_date_yyyy_mm_dd = max_date.strftime("%Y-%m-%d")
+# st.markdown(max_date_yyyy_mm_dd)
 
-st.write("Streamlit version: ", st.__version__)
+# st.write("Streamlit version: ", st.__version__)
